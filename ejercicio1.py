f97b6cd7421f7a51ae6181df50a5388d343774fa
valauaca-simulacroexamen-5vi03kz9 <valauaca-simulacroexamen-5vi03kz9@local>
2018-05-24 19:24:05 +0000

    finalisimo

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,8 +1,9 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
 import numpy as np
-x = np.linspace(0,10,10)
 
-for i in range(len(x)):
-    s = x[9-i]
-    print(s)
+nume=10
+for i in range(0,nume+1):
+     print(nume-i)
+        
+print("Hasta la vista, Baby")
