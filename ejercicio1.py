5fd3b379255092c55a62008bc494fe3831504ac3
angelicarjs-simulacroexamen-38dosipt <angelicarjs-simulacroexamen-38dosipt@local>
2018-05-24 19:03:52 +0000

    holi

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,8 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
+import numpy as np
+x = np.linspace(0,10,10)
+
+for i in range(len(x)):
+    s = x[9-i]
+    print(s)
